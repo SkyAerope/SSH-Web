cb75de57298bafa2ae616f4761dc9201133b030d
SkyAerope <81958172+SkyAerope@users.noreply.github.com>
2023-07-19 17:10:09 +0800

    精简

--- app.js
+++ app.js
@@ -1,75 +1,46 @@
 const express = require('express');
 const { Client } = require('ssh2');
 const http = require('http');
-const socketIO = require('socket.io');
-const ansiHTML = require('ansi-html');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.post('/execute', (req, res) => {
+  const command = req.body.command;
+  const conn = new Client();
 
-app.get('/main.css', (req, res) => {
-    res.sendFile(__dirname + '/main.css');
-});
+  conn.on('ready', () => {
+    conn.exec(command, (err, stream) => {
+      if (err) {
+        res.send('错误：' + err.message);
+        conn.end();
+        return;
+      }
 
-app.get('/favicon.svg', (req, res) => {
-    res.sendFile(__dirname + '/favicon.svg');
-});
-
-// 不需要猫咪背景图片了~
-// app.get('/cat.jpg', (req, res) => {
-//     res.sendFile(__dirname + '/cat.jpg');
-// });
-
-io.on('connection', (socket) => {
-    //获取当前时间
-  var date = new Date();
-    //获取客户端IP地址
-  var clientIp = socket.request.connection.remoteAddress;
-  console.log('[' + date.toLocaleString() + '] ' + clientIp + ' 已连接 ');
-
-
-  socket.on('execute', (command, host, port, username, password) => {
-    const conn = new Client();
-
-    conn.on('ready', () => {
-      conn.exec(command, (err, stream) => {
-        if (err) {
-          socket.emit('output', '错误：' + err.message);
-          conn.end();
-          return;
-        }
-
-        stream.on('close', (code, signal) => {
-          conn.end();
-          socket.disconnect(); // 断开连接
-        }).on('data', (data) => {
-          socket.emit('output', ansiHTML(data.toString()));
-        }).stderr.on('data', (data) => {
-          socket.emit('output', ansiHTML(data.toString()));
-        });
+      stream.on('close', (code, signal) => {
+        res.send(output);
+        conn.end();
+      }).on('data', (data) => {
+        output += data.toString();
+      }).stderr.on('data', (data) => {
+        output += data.toString();
       });
     });
+  });
 
-    conn.on('error', (err) => {
-      socket.emit('output', 'SSH 连接错误：' + err.message);
-    });
+  conn.on('error', (err) => {
+    res.send('SSH 连接错误：' + err.message);
+  });
 
-    conn.connect({
-        host: host,
-        port: port,
-        username: username,
-        password: password
-      });
+  conn.connect({
+    host: host,
+    port: port,
+    username: username,
+    password: password
   });
 });
 
